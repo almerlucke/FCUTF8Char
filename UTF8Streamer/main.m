352a4f83bb047d34fdb521a2991732c4ed6ba88a
Almer Lucke <almer.lucke@gmail.com>
2013-10-15 22:34:32 +0200

    Initial Commit

---
+++ UTF8Streamer/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  UTF8Streamer
+//
+//  Created by Almer Lucke on 10/15/13.
+//  Copyright (c) 2013 Farcoding. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
